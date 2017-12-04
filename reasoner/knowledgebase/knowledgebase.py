92da570ce4b615af360cf8bae56e155a20e300c3
Aditya Shinde <shinde.aditya386@gmail.com>
2017-12-04 02:05:22 -0500

    Added base KB structures

--- reasoner/knowledgebase/knowledgebase.py
+++ reasoner/knowledgebase/knowledgebase.py
@@ -2,6 +2,8 @@ import logging
 
 logger=logging.getLogger(__name__)
 
+from .axioms import Assertion
+
 class NodeSet(object):
     '''
         A set data structure for a node in the completion graph
@@ -10,7 +12,7 @@ class NodeSet(object):
     def __init__(self,name="Unnamed"):
         self.name=name
         self.axiom_set=set()
-        logger.debug("Empty NodeSet initialised.")
+        logger.debug(f"Empty NodeSet {self.name} initialised.")
 
     def add_axiom(self,axiom):
         '''
@@ -40,4 +42,102 @@ class NodeSet(object):
         '''
             Checks if the Set contains the given axiom.
         '''
+        logger.debug(f"Looking for {axiom} in {self.name}")
         return axiom in self.axiom_set
+
+class Box(NodeSet):
+    '''
+        Defines base object for ABox and TBox.
+    '''
+
+    def __init__(self,name):
+        super().__init__(name=name)
+
+    def extend(self,axioms):
+        '''
+            Adds a list of given axioms to the box.
+        '''
+        assert type(axioms)==list, "Argument for Box.extend has to be a list."
+        self.add_axioms(axioms)
+
+    def get_generator(self):
+        '''
+            returns a generator for getting axioms.
+        '''
+        for i,axiom in enumerate(self.axiom_set):
+            logger.debug(f"{self.name} yielding axiom {axiom}")
+            yield axiom
+
+    def get_axioms(self):
+        '''
+            returns a list of all axioms in the box.
+        '''
+        return list(self.axiom_set)
+
+class ABox(Box):
+    '''
+        Defines ABox.
+    '''
+    def __init__(self):
+        super().__init__("abox")
+
+
+class TBox(Box):
+    '''
+        Defines TBox.
+    '''
+    def __init__(self):
+        super().__init__("tbox")
+
+
+class KnowledgeBase(object):
+    '''
+        Defines the KB.
+    '''
+
+    def __init__(self):
+        self.abox=ABox()
+        self.tbox=TBox()
+        logger.debug(f"Knowledge base initialised.")
+
+    def __axiom_adder(self,axiom):
+        if type(axiom)==Assertion:
+            self.abox.add_axiom(axiom)
+        else:
+            self.tbox.add_axiom(axiom)
+
+    def add_axioms(self,axiom_list):
+        for axiom in axiom_list:
+            self.__axiom_adder(axiom)
+
+    def contains(self,axiom):
+        '''
+            returns whether the KB contains the given axiom.
+        '''
+        return self.abox.contains(axiom) or self.tbox.contains(axiom)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/test_kb.py
+++ tests/test_kb.py
@@ -5,34 +5,24 @@ import logging
 #logging.basicConfig(level=logging.DEBUG)
 sys.path.append("/home/adityas/Projects/ALC-reasoner/")
 
-from reasoner.knowledgebase.knowledgebase import NodeSet
-from reasoner.knowledgebase.axioms import And,Or,Not
-from reasoner.common.constructors import Concept,Some,All
+from reasoner.knowledgebase.knowledgebase import NodeSet,KnowledgeBase
+from reasoner.knowledgebase.axioms import And,Or,Not,Assertion
+from reasoner.common.constructors import Concept,Some,All,Instance
 
 class TestKB(unittest.TestCase):
 
     def setUp(self):
-        self.KB=NodeSet()
+        self.KB=KnowledgeBase()
         self.axioms=[
-            And(Concept("Man"),All("hasChild",Concept("Human"))),
-            And(Concept("Woman"),All("hasChild",Concept("Human"))),
-            Concept("Human"),
-            Not(Concept("Robot"))
+            Assertion(And(Concept("Man"),Concept("Human")),Instance("Aditya")),
+            Assertion(Some("owns",Concept("Conputer")),Instance("Aditya"))
         ]
+
+    def test_axiom_insertion(self):
         self.KB.add_axioms(self.axioms)
-
-    def test_add_multiple_axioms(self):
-        self.assertEqual(len(self.KB.axiom_set),len(self.axioms))
-
-    def test_add_single_axiom(self):
-        self.KB.add_axiom(Concept("Terminator"))
-        self.assertEqual(len(self.KB.axiom_set),len(self.axioms)+1)
-
-    def test_contains_axiom(self):
-        self.assertTrue(self.KB.contains(Concept("Human")))
-
-    def test_not_contains_axiom(self):
-        self.assertFalse(self.KB.contains(Concept("Dinosaur")))
+        #print(f"{self.KB.abox.axiom_set},{self.KB.tbox.axiom_set}")
+        self.assertTrue(self.KB.contains(self.axioms[0]))
+        self.assertTrue(self.KB.contains(self.axioms[1]))
 
 if __name__=="__main__":
     unittest.main()
