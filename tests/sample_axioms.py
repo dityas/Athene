413960b9c8561de7200159ef4ad7a5fdd6b7acb1
Aditya Shinde <shinde.aditya386@gmail.com>
2017-12-10 23:28:48 -0500

    Tracking test axioms file.

---
+++ tests/sample_axioms.py
@@ -0,0 +1,29 @@
+import sys 
+
+sys.path.append("/home/adityas/Projects/ALC-reasoner/")
+
+from reasoner.knowledgebase.axioms import And,Or,Not,ClassAssertion,ABoxAxiom
+from reasoner.common.constructors import Concept,All,Some,Instance
+
+simple_or=Or(Concept("Man"),Concept("Machine"))
+complex_or=Or(Or(Concept("Man"),Concept("Machine")),Concept("Robot"))
+kinda_complicated_axiom=And(Or(And(Concept("Man"),Not(Concept("Machine"))),And(Concept("Machine"),Not(Concept("Man")))),Concept("Physical"))
+kinda_complicated_abox=ABoxAxiom(ClassAssertion(kinda_complicated_axiom,Instance("Aditya")))
+
+
+kinda_complicated_unsat_axiom=And(Or(And(Concept("Man"),Not(Concept("Machine"))),And(Concept("Machine"),Not(Concept("Man")))),And(Concept("Man"),Concept("Machine")))
+
+kinda_complicated_unsat_abox=ABoxAxiom(ClassAssertion(kinda_complicated_unsat_axiom,Instance("Aditya")))
+
+simple_and_abox=ABoxAxiom(ClassAssertion(And(Concept("Man"),Concept("Machine")),Instance("Aditya")))
+
+simple_or_abox=ABoxAxiom(ClassAssertion(simple_or,Instance("Aditya")))
+
+consistent_complex_abox=[
+ABoxAxiom(ClassAssertion(Or(And(Concept("Man"),Not(Concept("Machine"))),And(Concept("Machine"),Not(Concept("Man")))),Instance("Aditya"))),
+ABoxAxiom(ClassAssertion(And(Concept("Man"),Concept("Machine")),Instance("Arnold")))]
+
+inconsistent_complex_abox=[
+ABoxAxiom(ClassAssertion(Or(And(Concept("Man"),Not(Concept("Machine"))),And(Concept("Machine"),Not(Concept("Man")))),Instance("Aditya"))),
+ABoxAxiom(ClassAssertion(And(Concept("Man"),Not(Concept("Man"))),Instance("Arnold")))]
+
